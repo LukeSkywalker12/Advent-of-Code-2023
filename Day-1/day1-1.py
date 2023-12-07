389b71d747a055804a65abd0a61058e7645053ec
LukeSkywalker12 <125671172+LukeSkywalker12@users.noreply.github.com>
2023-12-07 14:58:11 +0100

    Add files via upload

---
+++ Day-1/day1-1.py
@@ -0,0 +1,23 @@
+input = open("Day-1/list.txt", "r")         # Liest die Liste ein
+
+gesamt = 0
+
+for line in input:                          # geht einmal durch die Liste durch
+    zahl = 0
+
+    for __ in line:                         # geht einmal von vorne durch die Zeile durch
+        if __.isdigit():                    # falls der Teil vom String (nur ein Buchstabe oder eine Zahl) eine Zahl ist
+            zahl = __                       # setzt er die Variable auf diese Zahl
+            break                           # und beendet die for-Schleife
+    
+    index = -1
+
+    while index >= len(line)*-1:            # geht einmal von hinten durch die Zeile durch
+        if line[index].isdigit():           # hier auch falls es eine Zahl ist
+            zahl += line[index]             # wird die Zahl dann der Variable hinzugefügt
+            break                           # und die Schleife beendet
+        index-=1
+    
+    gesamt += int(zahl)                     # dann wird die Zahl noch der Gesamtzahl hinzuaddiert
+
+print(gesamt)
---
+++ Day-1/day1-2.py
@@ -0,0 +1,49 @@
+input = open("Day-1/list.txt", "r")
+
+gesamt = 0
+
+numbers = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+def findZahl1(line):
+    index = len(line)
+    returnNumber = "0"
+
+    for number in numbers:                                          # Einmal für jede Nummer in der Liste durchgehen
+        indexOfNumber = line.find(number)                           # 'find' gibt den Index des ersten Punktes des Gefundenen Wertes zurück
+
+        if indexOfNumber != -1 and indexOfNumber < index:
+            index = indexOfNumber
+            if numbers.index(number) < 9 :
+                returnNumber = number
+            else:
+                returnNumber = str(numbers.index(number)-8)         # Beispiel: "two" hat index 10 -> 10-8 = 2  es wird "2" für zurückgegeben
+    
+    return returnNumber
+
+def findZahl2(line):
+    index = 0
+    returnNumber = "0"
+
+    for number in numbers:
+        indexOfNumber = line.rfind(number)                          # 'rfind' gibt index von dem hintersten gefunden Punkt des eingegebenen Wertes
+
+        if indexOfNumber != -1 and indexOfNumber > index:
+            index = indexOfNumber
+            if numbers.index(number) < 9 :
+                returnNumber = number
+            else:
+                returnNumber = str(numbers.index(number)-8)         # Beispiel: "two" hat index 10 -> 10-8 = 2  es wird "2" für zurückgegeben
+    
+    return returnNumber
+    
+count = 0
+
+for line in input:
+    print("Linie: ", line)
+    zahl = findZahl1(line) + findZahl2(line)
+    print("Zahl: ", zahl)
+    gesamt += int(zahl)
+    count+=1
+
+print(gesamt)
+print(count)
